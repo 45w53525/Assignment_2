39f7b71d1df795dd735e8f4e170ea73739ad3619
Rohit Sharma <pc@DESKTOP-H94GUCO>
2022-04-01 17:30:44 -0400

    Add project files.

---
+++ SchoolProject/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolProject.Models
+{
+    public class Teacher
+    {
+        public int TeacherId;
+        public string TeacherFname;
+        public string TeacherLname;
+        public string TeacherNumber;
+        public string TeacherSalary;
+
+    }
+}
